5dc980c0d7fc46f821970ac2c738f69d3b44ff16
dinhtrungcalisthenic <34514221+dinhtrungcalisthenic@users.noreply.github.com>
2026-07-01 15:50:09 +0700

    Delete pages/api/verify-domain.js

--- pages/api/verify-domain.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const { challenge } = req.query;
-  
-  if (challenge) {
-    return res.status(200).json({ challenge });
-  }
-  
-  res.status(200).json({ message: "Verify endpoint OK" });
-}
